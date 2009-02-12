c6c6d5d5f59b7095a92745985cb441a2c11b4523
Andreas Heger <andreas.heger@gmail.com>
2009-02-12 18:06:25 +0000

    fixed path linking bug

--- python/setup.py
+++ python/setup.py
@@ -3,9 +3,9 @@ from distutils.extension import Extension
 import os.path
 import sys, glob
 
-include_dirs = ["/usr/include",".","../alignlib", ".."]
-libraries=["boost_python"]
-library_dirs=['/usr/lib']
+include_dirs = ["/usr/include",".","../alignlib", ".." ]
+libraries=["boost_python", "alignlib"]
+library_dirs=['/usr/lib', "../alignlib/.libs" ]
 
 files = glob.glob( "modules/*.cpp" )
 
